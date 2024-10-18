e9c0be1f14738fb282eef40eddd75e4e8394ba36
Malaka-MRT <malakabot1@gmail.com>
2024-10-18 22:54:19 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,10 +6,10 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
-MONGODB: process.env.MONGODB|| "",
+MONGODB: process.env.MONGODB || "",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/f1HSsCt/20241013-212427.jpg",
 ALIVE_MSG: process.env.ALIVE_MSG || "Hello, I am YourName i am alive now!",
 AUTO_READ_CMD: process.env.AUTO_READ_CMD || "true",
 AUTO_BIO:"true",
-AUTO_VOICE:"true"
+AUTO_VOICE:"true",
 };
